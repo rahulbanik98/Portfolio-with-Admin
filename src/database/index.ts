843ff43d6cde89a9bad8b58f94a49a419adad77d
rahulbanik98 <59626411+rahulbanik98@users.noreply.github.com>
2024-06-09 03:05:25 +0530

    work on admin part

---
+++ src/database/index.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export default async function cunnectToDB() {
+    try {
+        await mongoose.connect('mongodb+srv://rahulbanik98:rahulbanik98@cluster0.fnn6tpv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+        console.log("db connect sussesfully");
+
+    } catch (error) {
+        console.log("error in connection function ==>> ", error);
+    }
+}
