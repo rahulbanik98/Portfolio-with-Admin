01b7a5ecaccb03c519b0bdb6241f6fe2a5612fa4
rahulbanik98 <59626411+rahulbanik98@users.noreply.github.com>
2024-06-23 03:55:27 +0530

    set the nav route

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,43 +1,62 @@
+"use client"
 import Admincontactview from '@/components/adminview/contact';
-import React from 'react'
-import { Adminhome } from '@/components/adminview';
+import React, { useState } from 'react'
+import { Adminhome, Adminabout, Adminexperience, Admieducation, Adminporject, Admincontact } from '@/components/adminview';
+
+interface menuItemTypes {
+    id: string,
+    label: string,
+    component: React.ReactNode
+}
 
 const Adminview = () => {
-    const menuItem = [
+    const [selectedtabs, setSelectedTabs] = useState<string>("home")
+    const menuItem: menuItemTypes[] = [
         {
             id: 'home',
             label: 'Home',
-            component: <Adminhome/>
+            component: <Adminhome />
         }, {
             id: 'about',
-            label: 'About'
+            label: 'About',
+            component: <Adminabout />
         }, {
             id: 'experience',
-            label: 'Experience'
+            label: 'Experience',
+            component: <Adminexperience />
         }, {
             id: 'education',
-            label: 'Education'
+            label: 'Education',
+            component: <Admieducation />
         }, {
             id: 'project',
-            label: 'Project'
+            label: 'Project',
+            component: <Adminporject />
         }, {
             id: 'contact',
             label: 'Contact',
-            component: <Admincontactview/>
+            component: <Admincontact />
         }
     ]
+
+    const selectedNavTabsFunction = (id: string) => {
+        setSelectedTabs(id)
+    }
+
     return (
         <>
             <div className='border-b border-gray-200'>
                 <nav className='-mb-0.5 flex justify-center spcae-x-6' role='tablist'>
                     {menuItem?.map((value) => (
-                        <button key={value?.id} type='button' className='p-4 font-bold text-xl text-black'>
+                        <button key={value?.id} type='button' className='p-4 font-bold text-xl text-black' onClick={() => selectedNavTabsFunction(value.id)}>
                             {value?.label}
                         </button>
                     ))}
                 </nav>
                 <div className='mt-10'>
-16 min
+                    {
+                        menuItem?.map(item => item?.id === selectedtabs && item?.component)
+                    }
                 </div>
             </div>
         </>
--- src/components/adminview/index.ts
+++ src/components/adminview/index.ts
@@ -5,5 +5,29 @@ const Adminhome = dynamic(() => import('../adminview/home/index'), {
     // loading: () => <p>Loading...</p>,
 })
 
+const Adminabout = dynamic(() => import('../adminview/about/index'), {
+    ssr: true,
+    // loading: () => <p>Loading...</p>,
+})
 
-export { Adminhome, }
+const Admincontact = dynamic(() => import('../adminview/contact/index'), {
+    ssr: true,
+    // loading: () => <p>Loading...</p>,
+})
+
+const Admieducation = dynamic(() => import('../adminview/education/index'), {
+    ssr: true,
+    // loading: () => <p>Loading...</p>,
+})
+
+const Adminexperience = dynamic(() => import('../adminview/experience/index'), {
+    ssr: true,
+    // loading: () => <p>Loading...</p>,
+})
+
+const Adminporject = dynamic(() => import('../adminview/porject/index'), {
+    ssr: true,
+    // loading: () => <p>Loading...</p>,
+})
+
+export { Adminhome, Adminabout, Admincontact, Admieducation, Adminexperience, Adminporject}
