127b0ab4a5f6dfc3e1ddb000d267ac9816dee785
rahulbanik98 <59626411+rahulbanik98@users.noreply.github.com>
2024-06-23 03:24:24 +0530

    restart

--- src/database/index.ts
+++ src/database/index.ts
@@ -4,7 +4,6 @@ export default async function cunnectToDB() {
     try {
         await mongoose.connect('mongodb+srv://rahulbanik98:rahulbanik98@cluster0.fnn6tpv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
         console.log("db connect sussesfully");
-
     } catch (error) {
         console.log("error in connection function ==>> ", error);
     }
